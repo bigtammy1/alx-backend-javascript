2752f25054fd9086fb52ec322e58a7a920d850bf
bigtammy1 <bigtammy1@gmail.com>
2023-06-27 21:39:33 +0000

    Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,9 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    resolve();
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const response = 'Api response';
+      resolve(response);
+      reject(new Error('API request failed'));
+    }, 2000);
   });
 }
-
